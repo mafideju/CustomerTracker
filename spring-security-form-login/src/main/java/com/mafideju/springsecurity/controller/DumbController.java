cd3023d5c1c422559d7f63d8d16005a57300c7eb
Marcio Rodrigues <mafideju@outlook.com>
2022-02-04 23:19:27 -0300

    feat(root): boilerplate - spring security

---
+++ spring-security-form-login/src/main/java/com/mafideju/springsecurity/controller/DumbController.java
@@ -0,0 +1,13 @@
+package com.mafideju.springsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DumbController {
+
+	@GetMapping("/")
+	public String showHome() {
+		return "home";
+	}
+}
